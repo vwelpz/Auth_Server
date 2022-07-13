0ae3835668913283c8d68a693cfb8fa821031ad6
John Doe <johndoe@example.com>
2022-07-13 16:34:38 +0800

    Set Pool Size based on node env

--- database.js
+++ database.js
@@ -1,10 +1,12 @@
 // Handles Database Connection
 
+require('dotenv').config();
+
 const pg = require('pg');
 
 const pool = new pg.Pool({
     connectionString: process.env.DATABASE_URL,
-    max: process.env.POOL_SIZE,
+    max: process.env.NODE_ENV === "test" ? process.env.TEST_POOL_SIZE : process.env.POOL_SIZE,
     ssl: {
         rejectUnauthorized: true,
     },
